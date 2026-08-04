3a077999c886070aa51184de114dff7f31e3c738
Soumili-Code24 <soumili.code24@gmail.com>
2026-08-04 13:27:31 +0530

    Time: 2 ms (96.33%), Space: 45.8 MB (33.86%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> ds = new ArrayList<>();//use set if only once number neeed to store
+    rec(0, target, ds, candidates, ans);
+    return ans;
+  }
+
+  private void rec(int idx, int target, List<Integer> ds, int[] arr, List<List<Integer>> ans) {
+    if (idx == arr.length) {
+      if (target == 0) {
+        ans.add(new ArrayList<>(ds));
+      }
+      return;
+    }
+    if (arr[idx] <= target) {
+      ds.add(arr[idx]);
+      rec(idx, target - arr[idx], ds, arr, ans);
+      ds.remove(ds.size() - 1);
+    }
+    rec(idx + 1, target, ds, arr, ans);
+
+  }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
