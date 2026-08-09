762635ec51a5e71632e2a9390f6bd16a542d33b4
Soumili-Code24 <soumili.code24@gmail.com>
2026-08-09 13:46:18 +0530

    Sync LeetCode - 225. Implement Stack using Queues (Easy)

---
+++ Stack_Queue/225_ImplementStackUsingQueues.java
@@ -0,0 +1,42 @@
+/**
+ * Problem Link : https://leetcode.com/problems/implement-stack-using-queues/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+class MyStack {
+  private Deque<Integer> q;
+
+    public MyStack() {
+      q=new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        q.offer(x);
+        for(int i=0;i<q.size()-1;i++){
+          q.offer(q.poll());
+        }
+
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.size()==0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
