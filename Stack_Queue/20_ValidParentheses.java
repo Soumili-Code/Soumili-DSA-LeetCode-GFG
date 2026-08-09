0bceea8fcb819e7512aa40e465bd84df293a48a6
Soumili-Code24 <soumili.code24@gmail.com>
2026-08-09 19:40:04 +0530

    Sync LeetCode - 20. Valid Parentheses (Easy)

---
+++ Stack_Queue/20_ValidParentheses.java
@@ -0,0 +1,28 @@
+/**
+ * Problem Link : https://leetcode.com/problems/valid-parentheses/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> st = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            } else {
+                if(st.isEmpty())
+                    return false;
+                boolean isValidPattern = (st.peek() == '(' && s.charAt(i) == ')')
+                    || (st.peek() == '{' && s.charAt(i) == '}')
+                    || (st.peek() == '[' && s.charAt(i) == ']');
+                if (!isValidPattern) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
+
