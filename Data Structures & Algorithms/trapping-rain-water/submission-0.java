8f7aa00360dd51ee13a4b7345ffd75c2106525ce
Soumili-Code24 <soumili.code24@gmail.com>
2026-08-19 11:00:33 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int ans = 0,width=1;
+        int l=0,r=height.length-1;
+        int lmax=0,rmax=0;
+        while(l<r){
+            lmax = Math.max(lmax,height[l]);
+            rmax = Math.max(rmax,height[r]);
+            if(lmax < rmax){
+                ans += (lmax-height[l])*width;
+                l++;
+            }
+            else{
+                ans += (rmax-height[r])*width;
+                r--;
+            }
+        }
+        return ans;
+    }
+}
